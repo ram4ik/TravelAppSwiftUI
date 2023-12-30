3548150b19108918de5781c20265e2e79de768bd
ram4ik <ram4ik@hotmail.com>
2023-12-30 19:57:49 +0200

    Initial Commit

---
+++ TravelAppSwiftUI/TravelAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppSwiftUIApp.swift
+//  TravelAppSwiftUI
+//
+//  Created by Ramill Ibragimov on 30.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
